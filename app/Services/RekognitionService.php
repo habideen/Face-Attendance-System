b15c60069b0290da6215e960d385b5ef35d1ea10
Habideen Ibrahim <habideenibrahim@gmail.com>
2024-05-18 14:34:03 +0100

    implement service

--- app/Services/RekognitionService.php
+++ app/Services/RekognitionService.php
@@ -2,6 +2,52 @@
 
 namespace App\Services;
 
+use Aws\Rekognition\RekognitionClient;
+
 class RekognitionService
 {
+  protected $rekognition;
+
+  public function __construct()
+  {
+    $this->rekognition = new RekognitionClient([
+      'region' => config('aws.region'),
+      'version' => 'latest',
+      'credentials' => [
+        'key' => config('aws.credentials.key'),
+        'secret' => config('aws.credentials.secret'),
+      ],
+    ]);
+  }
+
+  public function indexFaces($image, $collectionId)
+  {
+    return $this->rekognition->indexFaces([
+      'CollectionId' => $collectionId,
+      'Image' => [
+        'Bytes' => $image,
+      ],
+      'ExternalImageId' => uniqid(),
+      'DetectionAttributes' => ['ALL'],
+    ]);
+  }
+
+  public function searchFacesByImage($image, $collectionId)
+  {
+    return $this->rekognition->searchFacesByImage([
+      'CollectionId' => $collectionId,
+      'Image' => [
+        'Bytes' => $image,
+      ],
+      'FaceMatchThreshold' => 95,
+      'MaxFaces' => 1,
+    ]);
+  }
+
+  public function createCollection($collectionId)
+  {
+    return $this->rekognition->createCollection([
+      'CollectionId' => $collectionId,
+    ]);
+  }
 }
