8ad0f89b60fa445e54a9668f2408f22fd8f1e1f7
Habideen Ibrahim <habideenibrahim@gmail.com>
2024-05-25 12:06:21 +0100

    add single student

--- resources/views/student/add_single.blade.php
+++ resources/views/student/add_single.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.panel')
 
-@section('page_title', 'Add Single Staff')
+@section('page_title', 'Add Single Student')
 
 
 @section('content')
@@ -11,12 +11,12 @@
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0 font-size-18">Add Single Staff</h4>
+                        <h4 class="mb-sm-0 font-size-18">Add Single Student</h4>
 
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
                                 <li class="breadcrumb-item"><a href="/{{ Request::segment(1) }}/dashboard">Dashboard</a></li>
-                                <li class="breadcrumb-item active">Add Single Staff</li>
+                                <li class="breadcrumb-item active">Add Single Student</li>
                             </ol>
                         </div>
                     </div>
@@ -44,11 +44,6 @@
                                         parentClass="mb-3 col-sm-6 col-md-3" placeholder="Enter Middle name" />
                                 </div>
 
-                                <div class="row mb-4">
-                                    <x-form.input name="address" label="Office Address" type="text" required='true'
-                                        parentClass="mb-3 col-12" placeholder="Enter Address" />
-                                </div>
-
                                 <div class="mb-4">
                                     <div class="h4 col-12">Contact</div>
                                     <div class="row mb-4">
@@ -70,14 +65,6 @@
                                         <x-form.input name="id" label="ID No." type="text" required='true'
                                             parentClass="mb-3 col-sm-6 col-md-3" placeholder="e.g CSC/1950/001" />
                                     </div>
-                                </div>
-
-                                <div class="row mb-4">
-                                    <div class="h4 col-12 mb-3">Select Roles</div>
-                                    <x-form.switch name="isAdmin" label="Admin" parentClass="mb-3 col-sm-4" />
-                                    <x-form.switch name="isStaffAdviser" label="Staff Adviser"
-                                        parentClass="mb-3 col-sm-4" />
-                                    <x-form.switch name="isLecturer" label="Lecturer" parentClass="mb-3 col-sm-4" />
                                 </div>
 
                                 <x-form.button defaultText="Add User" class="mt-4" />
--- routes/admin.php
+++ routes/admin.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Settings\SessionController;
 use App\Http\Controllers\Staff\AddStaffController;
 use App\Http\Controllers\Staff\LoadStaffController;
 use App\Http\Controllers\Staff\StaffController;
+use App\Http\Controllers\Student\AddStudentController;
 use App\Http\Controllers\Student\LoadStudentController;
 use Illuminate\Support\Facades\Route;
 
@@ -50,7 +51,7 @@ Route::prefix('admin')->group(function () {
 
   Route::prefix('students')->group(function () {
     Route::get('/', [StaffController::class, 'index']);
-    Route::get('add_student', [AddStaffController::class, 'index']);
+    Route::get('add_student', [AddStudentController::class, 'index']);
     Route::get('load_student', [LoadStudentController::class, 'index']);
     Route::get('details/{staff_id}', [StaffController::class, 'details']);
   });
