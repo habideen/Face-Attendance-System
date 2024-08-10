6c4cfa943c9787a342fe6056d5cf7252e92744dd
Habideen Ibrahim <habideenibrahim@gmail.com>
2024-08-10 17:45:04 +0100

    staff dashboard

---
+++ resources/views/staff/dashboard.blade.php
@@ -0,0 +1,136 @@
+@extends('layouts.panel')
+
+@section('page_title', 'Dashboard')
+
+
+@section('content')
+    <div class="page-content">
+        <div class="container-fluid">
+
+            <!-- start page title -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                        <h4 class="mb-sm-0 font-size-18">Dashboard</h4>
+
+                        <div class="page-title-right">
+                            <ol class="breadcrumb m-0">
+                                <li class="breadcrumb-item"><a href="/{{ Request::segment(1) }}/dashboard">Dashboard</a></li>
+                                <li class="breadcrumb-item active">Dashboard</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end page title -->
+
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card mini-stats-wid">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-grow-1">
+                                    <p class="text-muted fw-medium">Total Staff</p>
+                                    <h4 class="mb-0">{{ $total_staff }}</h4>
+                                </div>
+
+                                <div class="flex-shrink-0 align-self-center">
+                                    <div class="mini-stat-icon avatar-sm rounded-circle bg-primary">
+                                        <span class="avatar-title">
+                                            <i class="bx bxs-user-detail font-size-24"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mini-stats-wid">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-grow-1">
+                                    <p class="text-muted fw-medium">Total Student</p>
+                                    <h4 class="mb-0">{{ $total_student }}</h4>
+                                </div>
+
+                                <div class="flex-shrink-0 align-self-center ">
+                                    <div class="avatar-sm rounded-circle bg-primary mini-stat-icon">
+                                        <span class="avatar-title rounded-circle bg-primary">
+                                            <i class="bx bx-book font-size-24"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mini-stats-wid">
+                        <div class="card-body">
+                            <div class="d-flex">
+                                <div class="flex-grow-1">
+                                    <p class="text-muted fw-medium">Session Used</p>
+                                    <h4 class="mb-0">{{ Session::get('academic_session') }}</h4>
+                                </div>
+
+                                <div class="flex-shrink-0 align-self-center">
+                                    <div class="avatar-sm rounded-circle bg-primary mini-stat-icon">
+                                        <span class="avatar-title rounded-circle bg-primary">
+                                            <i class="bx bx-calendar font-size-24"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end row -->
+
+            <div class="row mt-3">
+                <div class="col-12">
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <x-alert />
+
+                            <table class="table table-hover">
+                                <tbody>
+                                    <tr>
+                                        <td>Student ID:</td>
+                                        <td>{{ Auth::user()->school_id }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Name:</td>
+                                        <td>{{ Auth::user()->sname . ' ' . Auth::user()->fname . ' ' . Auth::user()->mname }}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Email:</td>
+                                        <td>{{ Auth::user()->email }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Phone 1:</td>
+                                        <td>{{ Auth::user()->phone_1 }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Phone 2:</td>
+                                        <td>{{ Auth::user()->phone_2 }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Department:</td>
+                                        <td>{{ $myDepartment }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- row -->
+            <!-- card -->
+        </div>
+        <!-- container-fluid -->
+    </div>
+    <!-- End Page-content -->
+@endsection
